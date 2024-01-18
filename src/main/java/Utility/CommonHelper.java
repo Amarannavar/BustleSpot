10545d4e3b1720dbd17c67e93faf8d1d7f91b530
Shashidhar <shashidhar.amarannavar@softsuave.org>
2024-01-18 16:33:11 +0530

    Added new path

--- src/main/java/Utility/CommonHelper.java
+++ src/main/java/Utility/CommonHelper.java
@@ -21,7 +21,7 @@ public class CommonHelper {
 	public static Properties readPropertiesFile() throws IOException {
 
 		FileInputStream fis =new FileInputStream(
-				"C:\\Users\\Soft Suave\\BustlespotAutomation2024\\Bustlespot\\resources\\testConfig.properties");
+				"C:\\Users\\Softsuave\\BustlespotAutomation2024\\Bustlespot\\resources\\testConfig.properties");
 		Properties prop =new Properties();
 		prop.load(fis);
 		return prop;
--- src/test/java/TestRunner/LoginRun.java
+++ src/test/java/TestRunner/LoginRun.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\Soft Suave\\BustlespotAutomation2024\\Bustlespot\\Features", glue = {
+@CucumberOptions(features = "C:\\Users\\Softsuave\\BustlespotAutomation2024\\Bustlespot\\Features\\LoginScreen\\login.feature", glue = {
 		"stepDefinition" }, monochrome = true, dryRun = false, plugin = { "pretty",
 				"html:target/cucumber-reports.html" })
 
